1a26cb2e8f88701d72b96d03cf90a5b09dab8ece
froggal <keyfrog@outlook.kr>
2023-12-17 16:48:18 +0900

    Implement progress status display feature

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 # import modules
 from pytube import YouTube
 import ffmpeg
+print('Modules ready!')
+
+# infomation
+print('''
+__   __               _____         _                               _____  ______                          _                    _             
+\ \ / /              |_   _|       | |                             |____ | |  _  \                        | |                  | |            
+ \ V /   ___   _   _   | |   _   _ | |__    ___   _ __ ___   _ __      / / | | | |  ___  __      __ _ __  | |  ___    __ _   __| |  ___  _ __ 
+  \ /   / _ \ | | | |  | |  | | | || '_ \  / _ \ | '_ ` _ \ | '_ \     \ \ | | | | / _ \ \ \ /\ / /| '_ \ | | / _ \  / _` | / _` | / _ \| '__|
+  | |  | (_) || |_| |  | |  | |_| || |_) ||  __/ | | | | | || |_) |.___/ / | |/ / | (_) | \ V  V / | | | || || (_) || (_| || (_| ||  __/| |   
+  \_/   \___/  \__,_|  \_/   \__,_||_.__/  \___| |_| |_| |_|| .__/ \____/  |___/   \___/   \_/\_/  |_| |_||_| \___/  \__,_| \__,_| \___||_|   
+                                                            | |                                                                               
+                                                            |_|                                                                               
+
+''')
+print('Made by KeyFrog')
 
 # input music's info
 title = input(' Input music\'s title : ')
@@ -10,9 +25,11 @@ link = input(' Input music\'s YouTube URL : ')
 # download video from YouTube
 yt = YouTube(link)
 yt.streams.filter(only_audio=True).first().download()
+print(' DOWNLOAD SUCCESS!')
 
 # convert mp4 file to mp3
 input = ffmpeg.input(f'{yt.streams.filter(only_audio=True).first().download()}')
 audio = input.audio.filter("aecho", 0.8, 0.9, 1000, 0.3)
 out = ffmpeg.output(audio, f'{title} - {artist}.mp3')
 ffmpeg.run(out)
+print(' CONVERT SUCCESS!')
